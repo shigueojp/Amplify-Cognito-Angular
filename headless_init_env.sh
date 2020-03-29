dda1866a3d9ca948bc68567b30cfc74ec19dc40d
shigueojp <okuhamail@gmail.com>
2020-03-29 16:03:56 -0300

    Added config for prod test

---
+++ headless_init_env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+IFS='|'
+
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":true,\
+\"profileName\":\"default\"\
+}"
+AMPLIFY="{\
+\"envName\":\"test\"\
+}"
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+
+amplify init \
+--amplify $AMPLIFY \
+--providers $PROVIDERS \
+--yes
